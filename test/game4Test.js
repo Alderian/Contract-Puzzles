3b52ce3e4239b0ffacc62d6c6f61256c18053251
Alderian Annatar <53636+Alderian@users.noreply.github.com>
2022-12-23 10:20:23 -0300

    Game 4

--- test/game4Test.js
+++ test/game4Test.js
@@ -1,21 +1,29 @@
-const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
-const { assert } = require('chai');
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { assert } = require("chai");
 
-describe('Game4', function () {
+describe("Game4", function () {
   async function deployContractAndSetVariables() {
-    const Game = await ethers.getContractFactory('Game4');
+    const Game = await ethers.getContractFactory("Game4");
     const game = await Game.deploy();
 
-    return { game };
+    const signer1 = ethers.provider.getSigner(0);
+    const signer2 = ethers.provider.getSigner(1);
+
+    return { game, signer1, signer2 };
   }
-  it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+  it("should be a winner", async function () {
+    const { game, signer1, signer2, signer3 } = await loadFixture(
+      deployContractAndSetVariables
+    );
 
     // nested mappings are rough :}
+    const address1 = await signer1.getAddress();
+    await game.connect(signer2).write(address1);
 
-    await game.win();
+    const address2 = await signer2.getAddress();
+    await game.win(address2);
 
     // leave this assertion as-is
-    assert(await game.isWon(), 'You did not win the game');
+    assert(await game.isWon(), "You did not win the game");
   });
 });
