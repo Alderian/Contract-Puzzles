e91d7e096334d1e71614ce2a54bf7a3072ac543f
Alderian Annatar <53636+Alderian@users.noreply.github.com>
2022-12-23 10:58:17 -0300

    Game 5

--- test/game5Test.js
+++ test/game5Test.js
@@ -1,21 +1,37 @@
-const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
-const { assert } = require('chai');
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { assert } = require("chai");
 
-describe('Game5', function () {
+describe("Game5", function () {
   async function deployContractAndSetVariables() {
-    const Game = await ethers.getContractFactory('Game5');
+    const Game = await ethers.getContractFactory("Game5");
     const game = await Game.deploy();
 
     return { game };
   }
-  it('should be a winner', async function () {
+  it("should be a winner", async function () {
     const { game } = await loadFixture(deployContractAndSetVariables);
 
     // good luck
+    const threshold = 0x00ffffffffffffffffffffffffffffffffffffff;
+    let i = 0;
+    let isValid;
+    let lastSigner;
+    let address;
+    //loop until find valid address
+    while (!isValid) {
+      lastSigner = ethers.provider.getSigner(i);
+      address = await lastSigner.getAddress();
+      if (address < threshold) {
+        isValid = true;
+      }
+      i++;
+    }
 
-    await game.win();
+    console.log("tried accounts", i);
+    console.log("using account", address);
+    await game.connect(lastSigner).win();
 
     // leave this assertion as-is
-    assert(await game.isWon(), 'You did not win the game');
+    assert(await game.isWon(), "You did not win the game");
   });
 });
